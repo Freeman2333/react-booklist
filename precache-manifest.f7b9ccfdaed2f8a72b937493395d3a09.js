a13053e3d31c307125c9aa2c4f06a2038edb5b64
freeman <freeman23@bigmir.net>
2019-07-13 13:10:56 +0300

    Updates

---
+++ precache-manifest.f7b9ccfdaed2f8a72b937493395d3a09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ae03285dee78e8b678108b7ebb312fa",
+    "url": "/react-booklist/index.html"
+  },
+  {
+    "revision": "9ec9083718a08563f6f0",
+    "url": "/react-booklist/static/css/main.95f415ac.chunk.css"
+  },
+  {
+    "revision": "10bf244ca692441702db",
+    "url": "/react-booklist/static/js/2.1b3a0643.chunk.js"
+  },
+  {
+    "revision": "9ec9083718a08563f6f0",
+    "url": "/react-booklist/static/js/main.5d59ba08.chunk.js"
+  },
+  {
+    "revision": "67c6d2f263f22754b8af",
+    "url": "/react-booklist/static/js/runtime~main.98544cd3.js"
+  }
+]);
